314bc9f763ccba296452349a4da441bc4ac71f7f
Ami Lian <amiamilian99@gmail.com>
2025-10-27 14:34:39 -0400

    seed terms

--- server/src/db/seed.sql
+++ server/src/db/seed.sql
@@ -21,3 +21,15 @@ INSERT INTO majors (code, name) VALUES
 ('BCHM','Biochemistry')
 ON CONFLICT (code) DO NOTHING;
 
+-- ---------
+-- Terms
+-- ---------
+INSERT INTO terms (code, starts_on, ends_on) VALUES
+('FALL23',   DATE '2023-08-28', DATE '2023-12-15'),
+('SPRING24', DATE '2024-01-22', DATE '2024-05-10'),
+('FALL24',   DATE '2024-08-26', DATE '2024-12-13'),
+('SPRING25', DATE '2025-01-21', DATE '2025-05-09'),
+('FALL25',   DATE '2025-08-25', DATE '2025-12-12'),
+('SPRING26', DATE '2026-01-23', DATE '2026-05-08')
+ON CONFLICT (code) DO NOTHING;
+
