ce356a36c377984d0041648b5d8d0c20fe43889c
Ami Lian <amiamilian99@gmail.com>
2025-10-27 13:47:11 -0400

    user-major related routes

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -10,5 +10,9 @@ r.get('/:id', requireAuth, requireSelfOrAdmin(), c.getUser);
 r.patch('/:id', requireAuth, requireSelfOrAdmin(), c.updateUser);
 r.delete('/:id', requireAuth, requireAdmin, c.deleteUser);
 
-
+r.get('/:id/majors', requireAuth, requireSelfOrAdmin(), c.listUserMajors);
+r.post('/:id/majors', requireAuth, requireSelfOrAdmin(), c.addUserMajor);
+r.delete('/:id/majors/:code', requireAuth, requireSelfOrAdmin(), c.removeUserMajor);
+r.post('/:id/majors/bulk', requireAuth, requireSelfOrAdmin(), c.addUserMajorsBulk);
+r.put('/:id/majors', requireAuth, requireSelfOrAdmin(), c.replaceUserMajors);
 export default r;
