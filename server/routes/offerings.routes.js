903b773171d527ace711648e15ce98d86edc6903
Ami Lian <amiamilian99@gmail.com>
2025-10-27 14:12:32 -0400

    offering GET w/ param routes

--- server/routes/offerings.routes.js
+++ server/routes/offerings.routes.js
@@ -11,12 +11,16 @@ r.delete('/:id', requireAuth, requireRole('admin', 'teacher'), c.deleteOffering)
 r.delete('/:offeringId/prereqs/:prereqOfferingId', requireAuth, requireRole('admin', 'teacher'), c.removeOfferingPrereq);
 r.post('/:offeringId/prereqs', requireAuth, requireRole('admin', 'teacher'), c.addOfferingPrereq);
 
-
 r.get('/my', requireAuth, c.getMyOfferings);
 r.get('/filter', requireAuth, c.offeringFilter);
 r.get('/eligibility/passed', requireAuth, c.studentHasPassedCourse);
 r.get('/eligibility/prereqs', requireAuth, c.allPrereqsMetForCourse);
 r.get('/course/:courseId', requireAuth, c.listOfferingsForCourse);
+
+r.get('/:id/eligible/:studentId', requireAuth, requireRole('admin','teacher'), c.eligibleForStudent);
+r.get('/:id/classmates/search', requireAuth, c.searchClassmates);
+
+
 
 
 export default r;
