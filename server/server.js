b7e4e178dbfca9b408e2e30f6fba4c4565c1a935
Ami Lian <amiamilian99@gmail.com>
2025-10-27 14:33:34 -0400

    server.js set up

--- server/server.js
+++ server/server.js
@@ -4,6 +4,19 @@ import cors from "cors";
 import morgan from "morgan";
 import { config } from "./src/config/env.js";
 
+import authRoutes from "./src/routes/auth.routes.js";
+import usersRoutes from "./src/routes/users.routes.js";
+import coursesRoutes from "./src/routes/courses.routes.js";
+import assignmentsRoutes from "./src/routes/assignments.routes.js";
+import enrollmentsRoutes from "./src/routes/enrollments.routes.js";
+import gradesRoutes from "./src/routes/grades.routes.js";
+import materialsRoutes from "./src/routes/materials.routes.js";
+import offeringsRoutes from "./src/routes/offerings.routes.js";
+import submissionsRoutes from "./src/routes/submissions.routes.js";
+
+import notFound from "./src/middleware/notFound.js";
+import errorHandler from "./src/middleware/error.js";
+
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -13,6 +26,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: config.CORS_ORIGIN, credentials: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/courses", coursesRoutes);
+app.use("/api/assignments", assignmentsRoutes);
+app.use("/api/enrollments", enrollmentsRoutes);
+app.use("/api/grades", gradesRoutes);
+app.use("/api/materials", materialsRoutes);
+app.use("/api/offerings", offeringsRoutes);
+app.use("/api/submissions", submissionsRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const port = Number(config.PORT) || 8888;
 app.listen(port, () => {
